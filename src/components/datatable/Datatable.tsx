5aea9258f6837467c58b65a6a793afa6096c4e3f
JeefKeef <nguyen.jeffreyson@gmail.com>
2022-04-22 10:30:37 -0700

    Added Datatable component

---
+++ src/components/datatable/Datatable.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import './datatable.scss';
+import { userColumns, userRows } from '../../data/datatablesource';
+import { DataGrid } from '@mui/x-data-grid';
+
+const Datatable = () => {
+  const actionColumn = [
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 200,
+      renderCell: () => {
+        return (
+          <div className="cellAction">
+            <div className="viewButton">View</div>
+            <div className="deleteButton">Delete</div>
+          </div>
+        );
+      },
+    },
+  ];
+  return (
+    <div className="datatable">
+      <DataGrid
+        className="datagrid"
+        rows={userRows}
+        columns={userColumns.concat(actionColumn)}
+        pageSize={9}
+        rowsPerPageOptions={[9]}
+        checkboxSelection
+      />
+    </div>
+  );
+};
+
+export default Datatable;
